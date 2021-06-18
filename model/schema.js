f7c1a8b7b2baefdb01cfce2ce8d102754f1db976
Iliya <iliyamiroshnichenko88@gmail.com>
2021-06-18 19:19:10 +0300

    change project's structure

--- model/schema.js
+++ model/schema.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const { Schema, model } = mongoose;
 
 const contact = new Schema({
   name: {
@@ -18,6 +18,6 @@ const contact = new Schema({
   },
 });
 
-const Contacts = mongoose.model("contact", contact);
+const Contacts = model("contact", contact);
 
 module.exports = Contacts;
--- repositories/index.js
+++ repositories/index.js
@@ -1,4 +1,4 @@
-const Contacts = require("./schema");
+const Contacts = require("../model/schema");
 
 const listContacts = () => {
   return Contacts.find();
@@ -20,15 +20,10 @@ const updateContact = (id, body) => {
   return Contacts.findByIdAndUpdate({ _id: id }, body, { new: true });
 };
 
-const updateStatusContact = (id, body) => {
-  return Contacts.findByIdAndUpdate({ _id: id }, body, { new: true });
-};
-
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact,
-  updateStatusContact,
 };
